c62bbb5192b60b3a4275adeb79a336bb1ea92ab1
AakashKainth <aakash.kainth@gmail.com>
2024-11-19 10:14:54 -0500

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.write("This is a JS code");
+function factorial(n){
+    for (n=1; n<=10; n++ ){
+        return n * (factorial -1);
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
